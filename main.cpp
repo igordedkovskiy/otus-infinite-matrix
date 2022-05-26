4d4b36e2ec7098ddc50183486dec8342512ca222
Igor Dedkovskiy <divmy@yandex.ru>
2022-05-26 17:29:34 +0300

    Add files

---
+++ main.cpp
@@ -0,0 +1,8 @@
+// homework #5: infinite matrix.
+
+#include <vector>
+
+int main()
+{
+    return 0;
+}
---
+++ test_matrix.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include "gtest/gtest.h"
+//#include "print_ip.h"
+
+TEST(TEST_MATRIX, process_)
+{
+    ASSERT_TRUE(false);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
