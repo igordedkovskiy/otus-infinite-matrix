80dd48ee34653b92853df0187ea65bf6eed3e7b5
Igor Dedkovskiy <divmy@yandex.ru>
2022-05-27 15:10:29 +0300

    Add test to main.cpp

--- infinite_matrix.hpp
+++ infinite_matrix.hpp
@@ -1,6 +1,6 @@
 #include <type_traits>
-#include <vector>
 #include <unordered_map>
+#include <iterator>
 
 template<typename T> concept Integer = std::is_integral_v<T>;
 
@@ -9,28 +9,48 @@ template<Integer T, T default_v = 0> class InfiniteMatrix
 public:
     using size_type = std::size_t;
 
-    class Proxy
+    class row_t
     {
     public:
-        using row_t = std::unordered_map<size_type,T>;
+
+        class node_t
+        {
+        public:
+            T& operator=(const T& v)
+            {
+                element = v;
+            }
+        private:
+            T element;
+        };
+
+        using nested_row_t = std::unordered_map<size_type,T>;
 
         T& operator[](const T& key)
         {
             auto el = m_row.find(key);
             if(el == m_row.end())
-                ++m_size;
-            return m_row[key];
+            {
+                auto res = m_row.emplace(std::make_pair(key, default_v));
+                //if(!res.second)
+                //    throw?
+                el = res.first;
+            }
+            return el->second;
         }
 
-        Proxy& operator=(const Proxy&) = delete;
-        Proxy& operator=(Proxy&&) = delete;
+        row_t& operator=(const row_t&) = delete;
+        row_t& operator=(row_t&&) = delete;
+
+        size_type size() const noexcept
+        {
+            return m_row.size();
+        }
 
     private:
-        row_t m_row;
-        size_type m_size = 0;
+        nested_row_t m_row;
     };
 
-    using row_t = Proxy;
     row_t& operator[](const T& key)
     {
         return m_matrix[key];
@@ -38,15 +58,94 @@ public:
 
     size_type size() const noexcept
     {
-//        size_type sz = 0;
-//        for(const auto& it:m_matrix)
-//            sz += it.size();
-//        return sz;
-        return m_size;
+        size_type sz = 0;
+        for(const auto& it:m_matrix)
+            sz += it.second.size();
+        return sz;
     }
 
+
+//    struct Iterator
+//    {
+//        using value_type = T;
+//        using pointer = T*;
+//        using reference = T&;
+
+//        Iterator() noexcept = default;
+
+//        Iterator(pointer p) noexcept:
+//            ptr{p}
+//        {}
+
+//        Iterator(const Iterator& l) noexcept:
+//            ptr{l.ptr}
+//        {}
+
+//        ~Iterator() noexcept = default;
+
+//        Iterator& operator=(const Iterator& l) noexcept
+//        {
+//            ptr = l.ptr;
+//            return *this;
+//        }
+
+//        Iterator& operator++() noexcept
+//        {
+//            ++ptr;
+//            return *this;
+//        }
+
+//        Iterator& operator--() noexcept
+//        {
+//            --ptr;
+//            return *this;
+//        }
+
+//        reference operator*() const noexcept
+//        {
+//            return *ptr;
+//        }
+
+//        pointer operator->() const noexcept
+//        {
+//            return ptr;
+//        }
+
+//        friend bool operator==(const Iterator& l, const Iterator& r) noexcept
+//        {
+//            return l.ptr == r.ptr;
+//        }
+
+//        friend bool operator!=(const Iterator& l, const Iterator& r) noexcept
+//        {
+//            return l.ptr != r.ptr;
+//        }
+
+//    private:
+//        T* ptr = nullptr;
+//    };
+
+//    iterator begin() noexcept
+//    {
+//        return {m_start};
+//    }
+
+//    const_iterator begin() const noexcept
+//    {
+//        return {m_start};
+//    }
+
+//    iterator end() noexcept
+//    {
+//        return {m_end};
+//    }
+
+//    const_iterator end() const noexcept
+//    {
+//        return {m_end};
+//    }
+
 private:
-    using data_t = std::unordered_map<size_type, Proxy>;
+    using data_t = std::unordered_map<size_type, row_t>;
     data_t m_matrix;
-    size_type m_size = 0;
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // homework #5: infinite matrix.
 
-#include <vector>
+#include <iostream>
+#include <cassert>
 #include "infinite_matrix.hpp"
 
 int main()
@@ -9,5 +10,26 @@ int main()
     auto a = m[0][0];
     ++a;
 //    auto b = m[1];
+
+    {
+        InfiniteMatrix<int, -1> matrix;
+        assert(matrix.size() == 0); // все ячейки свободны
+
+        auto a = matrix[0][0];
+        assert(a == -1);
+        assert(matrix.size() == 0);
+        matrix[100][100] = 314;
+        assert(matrix[100][100] == 314);
+        assert(matrix.size() == 1);
+
+        // выведется одна строка
+        // 100100314
+//        for(auto c: matrix)
+//        {
+//            int x,y,v;
+//            std::tie(x, y, v) = c;
+//            std::cout << x << y << v << std::endl;
+//        }
+    }
     return 0;
 }
