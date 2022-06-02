8467cf9de914ea6dad79d37e450fd3419d84abf2
Igor Dedkovskiy <divmy@yandex.ru>
2022-06-02 12:29:21 +0300

    Add multidimensional version of InfiniteMatrix and tests

--- infinite_matrix.hpp
+++ infinite_matrix.hpp
@@ -2,11 +2,11 @@
 #include <unordered_map>
 #include <iterator>
 
-//template<typename T> struct TD;
+template<typename T> struct TD;
 
 template<typename T> concept Integer = std::is_integral_v<T>;
 
-template<Integer T, T default_v = 0> class InfiniteMatrix
+template<Integer T, std::size_t N, T default_v> class InfiniteMatrix
 {
 public:
     using size_type = std::size_t;
@@ -24,12 +24,211 @@ public:
     }
 
     /// \brief \a row_t is a wrapper, needed to overload operator[]
-    class row_t
+    template<std::size_t S, bool Dummy = true> class row_t
+    {
+    public:
+        using nested_row_t = std::unordered_map<size_type, row_t<S-1>>;
+
+        struct Iterator
+        {
+            using row_iterator = typename nested_row_t::iterator;
+            using el_iterator = typename nested_row_t::mapped_type::iterator;
+            using value_type = typename nested_row_t::value_type;
+            using pointer = typename nested_row_t::pointer;
+            nested_row_t& m_data;
+            row_iterator  m_row_it;
+            el_iterator   m_el_it;
+            using element_type = decltype(std::tuple_cat(std::tie(m_row_it->first), *m_el_it));
+            using reference = element_type;
+
+            Iterator() noexcept = delete;
+            Iterator(const Iterator&) noexcept = default;
+            Iterator(Iterator&&) noexcept = default;
+            //Iterator& operator=(const Iterator&) noexcept = default;
+            //Iterator& operator=(Iterator&&) noexcept = default;
+            ~Iterator() noexcept = default;
+
+            Iterator& operator=(const Iterator& l) noexcept
+            {
+                m_data = l.m_data;
+                m_row_it = l.m_row_it;
+                m_el_it = l.m_el_it;
+                return *this;
+            }
+            Iterator& operator=(Iterator&& l) noexcept
+            {
+                m_data = l.m_data;
+                m_row_it = std::move(l.m_row_it);
+                m_el_it = std::move(l.m_el_it);
+                return *this;
+            }
+
+            Iterator(nested_row_t& data, const row_iterator& row, const el_iterator& el) noexcept:
+                m_data{data},
+                m_row_it{row},
+                m_el_it{el}
+            {}
+
+            Iterator& operator++() noexcept
+            {
+                ++m_el_it;
+                if(m_el_it == std::end(m_row_it->second))
+                {
+                    ++m_row_it;
+                    if(m_row_it != std::end(m_data))
+                        m_el_it = std::begin(m_row_it->second);
+                }
+                return *this;
+            }
+
+            reference operator*() noexcept
+            {
+                return std::tuple_cat(std::tie(m_row_it->first), *m_el_it);
+            }
+
+//            pointer operator->() const noexcept
+//            {
+//                return &(m_el_it->second);
+//            }
+
+            friend bool operator==(const Iterator& l, const Iterator& r) noexcept
+            {
+                return (l.m_data == r.m_data) && (l.m_row_it == r.m_row_it) && (l.m_el_it == r.m_el_it);
+            }
+
+            friend bool operator!=(const Iterator& l, const Iterator& r) noexcept
+            {
+                return (l.m_data != r.m_data) || (l.m_row_it != r.m_row_it) || (l.m_el_it != r.m_el_it);
+            }
+        private:
+//            nested_row_t& m_data;
+//            row_iterator  m_row_it;
+//            el_iterator   m_el_it;
+        };
+
+        using iterator = Iterator;
+        using const_iterator = const Iterator;
+
+        row_t() noexcept = default;
+        row_t(const row_t&) noexcept = default;
+        row_t(row_t&&) noexcept = default;
+        row_t& operator=(const row_t&) noexcept = default;
+        row_t& operator=(row_t&&) noexcept = default;
+        ~row_t() noexcept = default;
+
+
+        iterator begin() noexcept
+        {
+            return {m_row, std::begin(m_row), std::begin(std::begin(m_row)->second)};
+        }
+
+        const_iterator begin() const noexcept
+        {
+            return {m_row, std::begin(m_row), std::begin(std::begin(m_row)->second)};
+        }
+
+        iterator end() noexcept
+        {
+            const auto row = std::next(std::begin(m_row), m_row.size()-1);
+            return {m_row, std::end(m_row), std::end(row->second)};
+        }
+
+        const_iterator end() const noexcept
+        {
+            const auto row = std::next(std::begin(m_row), m_row.size()-1);
+            return {m_row, std::end(m_row), std::end(row->second)};
+        }
+
+
+        friend bool operator==(const row_t& l, const row_t& r)
+        {
+            return l.m_row == r.m_row;
+        }
+
+        /// \note operator[] is overloaded to insert correct default cell value, which is impossible
+        ///       to achieve with nested_row_t::operator[]
+        row_t<S-1>& operator[](const size_type& key)
+        {
+            return m_row[key];
+        }
+
+        size_type size() const noexcept
+        {
+            size_type sz = 0;
+            for(const auto& it:m_row)
+                sz += it.second.size();
+            return sz;
+        }
+
+    private:
+        nested_row_t m_row;
+    };
+
+    template<bool Dummy> class row_t<1, Dummy>
     {
     public:
         using nested_row_t = std::unordered_map<size_type, T>;
-        using iterator = typename nested_row_t::iterator;
-        using const_iterator = const iterator;
+
+        struct Iterator
+        {
+            using el_iterator = typename nested_row_t::iterator;
+            using pointer = T*;
+            using element_type = std::tuple<size_type, T&>;
+            using reference = element_type;
+
+            Iterator() noexcept = delete;
+            Iterator(const Iterator&) noexcept = default;
+            Iterator(Iterator&&) noexcept = default;
+            Iterator& operator=(const Iterator&) noexcept = default;
+            Iterator& operator=(Iterator&&) noexcept = default;
+            ~Iterator() noexcept = default;
+
+//            Iterator& operator=(const Iterator& l) noexcept
+//            {
+//                m_el_it = l.m_el_it;
+//                return *this;
+//            }
+//            Iterator& operator=(Iterator&& l) noexcept
+//            {
+//                m_el_it = std::move(l.m_el_it);
+//                return *this;
+//            }
+
+            Iterator(const el_iterator& el) noexcept:
+                m_el_it{el}
+            {}
+
+            Iterator& operator++() noexcept
+            {
+                ++m_el_it;
+                return *this;
+            }
+
+            reference operator*() noexcept
+            {
+                return {m_el_it->first, m_el_it->second};
+            }
+
+            pointer operator->() const noexcept
+            {
+                return &(m_el_it->second);
+            }
+
+            friend bool operator==(const Iterator& l, const Iterator& r) noexcept
+            {
+                return l.m_el_it == r.m_el_it;
+            }
+
+            friend bool operator!=(const Iterator& l, const Iterator& r) noexcept
+            {
+                return l.m_el_it != r.m_el_it;
+            }
+        private:
+            el_iterator   m_el_it;
+        };
+
+        using iterator = Iterator;
+        using const_iterator = const Iterator;
 
         row_t() noexcept = default;
         row_t(const row_t&) noexcept = default;
@@ -95,18 +294,22 @@ public:
         nested_row_t m_row;
     };
 
-    using data_t = std::unordered_map<size_type, row_t>;
+
+    using data_t = std::unordered_map<size_type, row_t<N-1>>;
 
     struct Iterator
     {
         using row_iterator = typename data_t::iterator;
-        using el_iterator = typename data_t::mapped_type::iterator;
-        using value_type = T;
-        using pointer = T*;
-        using element_type = std::tuple<size_type, size_type, T&>;
+        using el_iterator = typename row_t<N-1>::iterator;
+//        using value_type = T;
+//        using pointer = T*;
+        data_t& m_data;
+        row_iterator m_row_it;
+        el_iterator m_el_it;
+        using element_type = decltype(std::tuple_cat(std::tie(m_row_it->first), *m_el_it));
         using reference = element_type;
 
-        Iterator() noexcept = default;
+        Iterator() noexcept = delete;
         Iterator(const Iterator&) noexcept = default;
         Iterator(Iterator&&) noexcept = default;
         Iterator& operator=(const Iterator& l) noexcept = default;
@@ -133,13 +336,13 @@ public:
 
         reference operator*() noexcept
         {
-            return {m_row_it->first, m_el_it->first, m_el_it->second};
+            return std::tuple_cat(std::tie(m_row_it->first), *m_el_it);
         }
 
-        pointer operator->() const noexcept
-        {
-            return &(m_el_it->second);
-        }
+//        pointer operator->() const noexcept
+//        {
+//            return &(m_el_it->second);
+//        }
 
         friend bool operator==(const Iterator& l, const Iterator& r) noexcept
         {
@@ -152,9 +355,9 @@ public:
         }
 
     private:
-        data_t& m_data;
-        row_iterator m_row_it;
-        el_iterator m_el_it;
+//        data_t& m_data;
+//        row_iterator m_row_it;
+//        el_iterator m_el_it;
     };
 
     using iterator = Iterator;
@@ -182,7 +385,7 @@ public:
         return {m_matrix, std::end(m_matrix), std::end(row->second)};
     }
 
-    row_t& operator[](const size_type& key)
+    row_t<N-1>& operator[](const size_type& key)
     {
         return m_matrix[key];
     }
@@ -198,3 +401,5 @@ public:
 private:
     data_t m_matrix;
 };
+
+
--- main.cpp
+++ main.cpp
@@ -5,30 +5,65 @@
 
 int main()
 {
-    InfiniteMatrix<unsigned int, 0> matrix;
-    for(std::size_t cntr = 0; cntr < 10; ++cntr)
-        matrix[cntr][cntr] = cntr;
-    for(std::size_t row = 0, col = 9; row < 10; ++row, --col)
-        matrix[row][col] = col;
-
-    std::cout << "\n[matrix[1][1] matrix[8][8]]:\n";
-    for(std::size_t row = 1; row < 9; ++row)
     {
-        for(std::size_t col = 1; col < 9; ++col)
-            std::cout << matrix[row][col] << ' ';
-        std::cout << '\n';
+        std::cout << "Two dimensional matrix:\n";
+        InfiniteMatrix<unsigned int, 2, 0> matrix;
+        for(std::size_t cntr = 0; cntr < 10; ++cntr)
+            matrix[cntr][cntr] = cntr;
+        for(std::size_t row = 0, col = 9; row < 10; ++row, --col)
+            matrix[row][col] = col;
+
+        std::cout << "\n[matrix[1][1] matrix[8][8]]:\n";
+        for(std::size_t row = 1; row < 9; ++row)
+        {
+            for(std::size_t col = 1; col < 9; ++col)
+                std::cout << matrix[row][col] << ' ';
+            std::cout << '\n';
+        }
+
+        std::cout << "\nNumber of occuppied cells is " << matrix.size() << '\n';
+
+        std::cout << "\nOccuppied cells and it's indices:\n";
+        for(const auto& it:matrix)
+        {
+            const auto [x,y,v] = it;
+            //unsigned int x,y,v;
+            //std::tie(x, y, v) = it;
+            if(v != matrix.default_value())
+                std::cout << "matrix[" << x << "][" << y << "] = " << v << std::endl;
+        }
     }
 
-    std::cout << "\nNumber of occuppied cells is " << matrix.size() << '\n';
-
-    std::cout << "\nOccuppied cells and it's indices:\n";
-    for(const auto& it:matrix)
     {
-        const auto [x,y,v] = it;
-        //unsigned int x,y,v;
-        //std::tie(x, y, v) = it;
-        if(v != matrix.default_value())
-            std::cout << "matrix[" << x << "][" << y << "] = " << v << std::endl;
+        std::cout << "Three dimensional matrix:\n";
+        InfiniteMatrix<unsigned int, 3, 0> matrix;
+        for(std::size_t cntr = 0; cntr < 10; ++cntr)
+            matrix[cntr][cntr][cntr] = cntr;
+        for(std::size_t row = 0, col = 9, zdim = 0; row < 10; ++row, --col, ++zdim)
+            matrix[row][col][zdim] = col;
+
+        std::cout << "\n[matrix[3][3][3] matrix[6][6][6]]:\n";
+        for(std::size_t row = 3; row < 6; ++row)
+        {
+            std::cout << "slice #" << row << "\n";
+            for(std::size_t col = 3; col < 6; ++col)
+            {
+                for(std::size_t zdim = 3; zdim < 6; ++zdim)
+                    std::cout << matrix[row][col][zdim] << ' ';
+                std::cout << '\n';
+            }
+            std::cout << '\n';
+        }
+
+        std::cout << "\nNumber of occuppied cells is " << matrix.size() << '\n';
+
+        std::cout << "\nOccuppied cells and it's indices:\n";
+        for(const auto& it:matrix)
+        {
+            const auto [x,y,z,v] = it;
+            if(v != matrix.default_value())
+                std::cout << "matrix[" << x << "][" << y << "][" << z << "] = " << v << std::endl;
+        }
     }
     return 0;
 }
